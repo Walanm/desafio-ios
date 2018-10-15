67523be39749f973faa74818ef3cab818a30c86e
Walan Marcel Teles Oliveira <walan.dn@gmail.com>
2018-10-14 23:06:56 -0300

    Adiciona layout do app

---
+++ Movs/MovieStore.swift
@@ -0,0 +1,9 @@
+//
+//  MovieStore.swift
+//  Movs
+//
+//  Created by Walan Marcel Teles Oliveira on 10/12/18.
+//  Copyright © 2018 Walan Marcel Teles Oliveira. All rights reserved.
+//
+
+import Foundation
